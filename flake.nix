08fcd103263221e8b0b1bda9676a839d594ce2b0
Timothy Klim <git@timothyklim.com>
2024-05-31 16:59:55 +0400

    chore: upgrade nixpkgs to nixos-24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Skia flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     # Build deps
